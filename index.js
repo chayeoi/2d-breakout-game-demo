431a7be005af295d77e4c12d659c60914ad37839
chayeoi <chayeoikeem@gmail.com>
2019-02-14 20:19:55 +0900

    feat: Step 5

--- index.js
+++ index.js
@@ -36,8 +36,16 @@ function draw() {
     dx = -dx
   }
 
-  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+  if (y + dy < ballRadius) {
     dy = -dy
+  } else if (y + dy > canvas.height - ballRadius) {
+    if (x > paddleX && x < paddleX + paddleWidth) {
+      dy = -dy
+    }
+    else {
+      alert('GAME OVER')
+      document.location.reload()
+    }
   }
 
   if (rightPressed && paddleX < canvas.width - paddleWidth) {
