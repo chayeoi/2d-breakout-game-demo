587668a898621c78dc52a9d5c305228e3a413328
chayeoi <chayeoikeem@gmail.com>
2019-02-14 20:34:07 +0900

    feat: Step 6

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const canvas = document.getElementById('myCanvas')
 const ctx = canvas.getContext('2d')
+
 const ballRadius = 10
+
 let x = canvas.width / 2
 let y = canvas.height - 30
 let dx = 2
 let dy = -2
+
 const paddleHeight = 10
 const paddleWidth = 75
 let paddleX = (canvas.width - paddleWidth) / 2
+
 let rightPressed = false
 let leftPressed = false
+
+const brickRowCount = 3
+const brickColumnCount = 5
+const brickWidth = 75
+const brickHeight = 20
+const brickPadding = 10
+const brickOffsetTop = 30
+const brickOffsetLeft = 30
+
+const bricks = []
+for (let c = 0; c < brickColumnCount; c++) {
+  bricks[c] = []
+  for (let r = 0; r < brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0 }
+  }
+}
 
 function drawBall() {
   ctx.beginPath()
@@ -27,8 +47,25 @@ function drawPaddle() {
   ctx.closePath()
 }
 
+function drawBricks() {
+  for (let c = 0; c < brickColumnCount; c++) {
+    for (let r = 0; r < brickRowCount; r++) {
+      const brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft
+      const brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop
+      bricks[c][r].x = brickX
+      bricks[c][r].y = brickY
+      ctx.beginPath()
+      ctx.rect(brickX, brickY, brickWidth, brickHeight)
+      ctx.fillStyle = '#0095DD'
+      ctx.fill()
+      ctx.closePath()
+    }
+  }
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
+  drawBricks()
   drawBall()
   drawPaddle()
 
