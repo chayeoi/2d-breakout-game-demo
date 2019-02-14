25f1318daf73680d54e88967d17a317c22a9c001
chayeoi <chayeoikeem@gmail.com>
2019-02-14 19:54:08 +0900

    feat: Step 4

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ let x = canvas.width / 2
 let y = canvas.height - 30
 let dx = 2
 let dy = -2
+const paddleHeight = 10
+const paddleWidth = 75
+let paddleX = (canvas.width - paddleWidth) / 2
+let rightPressed = false
+let leftPressed = false
 
 function drawBall() {
   ctx.beginPath()
@@ -14,9 +19,18 @@ function drawBall() {
   ctx.closePath()
 }
 
+function drawPaddle() {
+  ctx.beginPath()
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight)
+  ctx.fillStyle = '#0095DD'
+  ctx.fill()
+  ctx.closePath()
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   drawBall()
+  drawPaddle()
 
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dx = -dx
@@ -26,8 +40,36 @@ function draw() {
     dy = -dy
   }
 
+  if (rightPressed && paddleX < canvas.width - paddleWidth) {
+    paddleX += 7
+  }
+  else if (leftPressed && paddleX > 0) {
+    paddleX -= 7
+  }
+
   x += dx
   y += dy
+}
+
+document.addEventListener('keydown', keyDownHandler, false)
+document.addEventListener('keyup', keyUpHandler, false)
+
+function keyDownHandler(e) {
+  if (e.keyCode == 39) {
+    rightPressed = true
+  }
+  else if (e.keyCode == 37) {
+    leftPressed = true
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.keyCode == 39) {
+    rightPressed = false
+  }
+  else if (e.keyCode == 37) {
+    leftPressed = false
+  }
 }
 
 setInterval(draw, 10)
