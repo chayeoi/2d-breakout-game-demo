75e8dd4d37cb159750c3d228b0913950f1e338b3
chayeoi <chayeoikeem@gmail.com>
2019-02-14 18:50:55 +0900

    feat: Step 3

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById('myCanvas')
 const ctx = canvas.getContext('2d')
+const ballRadius = 10
 let x = canvas.width / 2
 let y = canvas.height - 30
 let dx = 2
@@ -7,7 +8,7 @@ let dy = -2
 
 function drawBall() {
   ctx.beginPath()
-  ctx.arc(x, y, 10, 0, Math.PI * 2)
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2)
   ctx.fillStyle = '#0095DD'
   ctx.fill()
   ctx.closePath()
@@ -16,6 +17,15 @@ function drawBall() {
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   drawBall()
+
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx
+  }
+
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    dy = -dy
+  }
+
   x += dx
   y += dy
 }
